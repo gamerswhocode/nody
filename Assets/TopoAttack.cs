684bfb62ab18dd24a1e228bcc53620939c95d872
Oscar Whizar <whizar.o@gmail.com>
2019-09-07 16:35:38 -0700

    Added Topo attack animation when player stands close

---
+++ Assets/TopoAttack.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TopoAttack : MonoBehaviour
+{
+    public float attackRange = 1f;
+    public Animator animator;
+
+    private Transform player;
+    private bool topoSpawned = false;
+    
+
+    private void Start()
+    {
+        player = GameObject.Find("Jugador").transform;
+    }
+    public void OnTopoSpawnComplete()
+    {
+        topoSpawned = true;
+    }
+    void Update()
+    {
+        if (!topoSpawned) return;
+
+        // Get distance to player
+        Vector3 directionVector = player.position - transform.position;
+        float distance = directionVector.magnitude;
+        if (distance <= attackRange)
+        {
+            if (animator.GetCurrentAnimatorStateInfo(0).IsName("TopoIdle"))
+            {
+                animator.Play("TopoAttack");
+            }
+        }
+
+    }
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawSphere(transform.position, attackRange);
+    }
+}
